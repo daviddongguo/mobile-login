4016ba614ae947a70a5dffc14f263947b8031138
daviddongguo <david.dong.guo@gmail.com>
2021-02-15 23:19:22 -0500

    Yarn lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <div>
-        <p>Hi, there!</p>
-      </div>
-    </div>
-  );
+// Written as a function declaration
+function App(): React.ReactNode {
+    return (
+        <div className="App">
+            <div>
+                <p>Hi, there!</p>
+            </div>
+        </div>
+    );
 }
 
 export default App;
