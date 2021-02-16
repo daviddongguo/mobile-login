ca130c22b25094d23dfabb40c3d951def74e3a32
daviddongguo <david.dong.guo@gmail.com>
2021-02-16 11:01:37 -0500

    Remove other react.node

--- src/components/Signin.tsx
+++ src/components/Signin.tsx
@@ -3,16 +3,16 @@ import { Button, Card, Form, Input } from 'reactstrap';
 
 // Written as a function declaration
 export default function Signin(): JSX.Element {
-  return (
-    <div>
-      <Card>
-        <p>Hi, there!</p>
-        <Form>
-          <Input value="Email"></Input>
-          <Input></Input>
-          <Button></Button>
-        </Form>
-      </Card>
-    </div>
-  );
+    return (
+        <div>
+            <Card>
+                <p>Hi, there!</p>
+                <Form>
+                    <Input value="Email"></Input>
+                    <Input></Input>
+                    <Button></Button>
+                </Form>
+            </Card>
+        </div>
+    );
 }
