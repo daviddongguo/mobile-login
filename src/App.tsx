d19a5fc7f23ff0e30d78c62e84bea057ad7d79ff
daviddongguo <david.dong.guo@gmail.com>
2021-02-16 07:52:00 -0500

    Create a card in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
+import { Button, Card, Col, Container, Form, Input, Row } from 'reactstrap';
 import './App.css';
 
 // Written as a function declaration
 function App(): JSX.Element {
     return (
         <div className="App">
-            <div>
-                <p>Hi, there!</p>
-            </div>
+            <Container className="signin-container">
+                <Row>
+                    <Col></Col>
+                    <Col className="signin-body" sm={8}>
+                        <Card>
+                            <p>Hi, there!</p>
+                            <Form>
+                                <Input value="Email"></Input>
+                                <Input></Input>
+                                <Button></Button>
+                            </Form>
+                        </Card>
+                    </Col>
+                    <Col></Col>
+                </Row>
+            </Container>
         </div>
     );
 }
